a4b315198754a6fe83b21cb73cfb0a6f1e1c5250
Winston R. Milling <Winston@Milli.ng>
2021-08-21 18:11:25 -0500

    SOPS: Migrate straglers to SOPS

--- scripts/setupK3sMaster.sh
+++ scripts/setupK3sMaster.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+REPO_ROOT=$(git rev-parse --show-toplevel)
+. "$REPO_ROOT"/.env
+
 echo "Building Master Node"
-qm clone 1000 400 --name k3s-amd64-0.homelab.tokyo --format raw --full --storage local-lvm
+qm clone 1000 400 --name k3s-amd64-0.${INTERNAL_DOMAIN} --format raw --full --storage local-lvm
 qm resize 400 scsi0 64G
 qm set 400 --boot c --startup order=3 -onboot 1 --bootdisk scsi0
 qm set 400 -cdrom /var/lib/vz/template/iso/k3s-seed-k3s-amd64-0.iso
--- scripts/setupK3sWorkers.sh
+++ scripts/setupK3sWorkers.sh
@@ -1,7 +1,10 @@
 #!/usr/bin/env bash
 
+REPO_ROOT=$(git rev-parse --show-toplevel)
+. "$REPO_ROOT"/.env
+
 echo "Building worker nodes"
-qm clone 1000 401 --name k3s-amd64-a.homelab.tokyo --format raw --full --storage local-lvm
+qm clone 1000 401 --name k3s-amd64-a.${INTERNAL_DOMAIN} --format raw --full --storage local-lvm
 qm resize 401 scsi0 64G
 qm set 401 --boot c --startup order=3 -onboot 1 --bootdisk scsi0
 qm set 401 -cdrom /var/lib/vz/template/iso/k3s-seed-k3s-amd64-a.iso
@@ -9,7 +12,7 @@ qm set 401 -scsi1 local-lvm:32
 qm set 401 --memory 16384
 qm start 401
 
-qm clone 1000 402 --name k3s-amd64-b.homelab.tokyo --format raw --full --storage local-lvm
+qm clone 1000 402 --name k3s-amd64-b.${INTERNAL_DOMAIN} --format raw --full --storage local-lvm
 qm resize 402 scsi0 64G
 qm set 402 --boot c --startup order=3 -onboot 1 --bootdisk scsi0
 qm set 402 -cdrom /var/lib/vz/template/iso/k3s-seed-k3s-amd64-b.iso
@@ -17,7 +20,7 @@ qm set 402 -scsi1 local-lvm:32
 qm set 402 --memory 16384
 qm start 402
 
-qm clone 1000 403 --name k3s-amd64-c.homelab.tokyo --format raw --full --storage local-lvm
+qm clone 1000 403 --name k3s-amd64-c.${INTERNAL_DOMAIN} --format raw --full --storage local-lvm
 qm resize 403 scsi0 64G
 qm set 403 --boot c --startup order=3 -onboot 1 --bootdisk scsi0
 qm set 403 -cdrom /var/lib/vz/template/iso/k3s-seed-k3s-amd64-c.iso
